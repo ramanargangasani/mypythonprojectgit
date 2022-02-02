e845d1bf617445af4c572b6f4f810b17de6cc5f2
Ramana <ramana.gangasani@gmail.com>
2022-02-02 11:37:35 -0600

    Y person commit to code

--- Examples/CallClassTest.py
+++ Examples/CallClassTest.py
@@ -1,6 +1,8 @@
 def Test123():
     a = 6
+    b= 7
     print("testing Call Base class method")
+    print(" modification completed the in the test case")
     return a
 
 Test123()
---
+++ Examples/CallPyTestmethods.py
@@ -0,0 +1,6 @@
+def pytest():
+    a = 6
+    print("testing pytest framework")
+    return a
+
+pytest()
